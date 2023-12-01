932101ac2a68d24ff4104b95b17af550e4301223
Md Nafis Iqbal <nafisiqbal.net2002@gmail.com>
2023-12-01 23:30:57 +0600

    first commit

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [titleText,setTitleText]=useState("Hello world");
+
+  const onPressChange = () =>{
+    setTitleText("Pressed");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.headerText} onPress={onPressChange}>{titleText}</Text>
+      <Text style={styles.subtext}>Another piece of text components</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +26,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  headerText: {
+    fontSize: 20,
+    color:'white',
+    paddingHorizontal: 6,
+    paddingVertical: 6,
+    borderRadius: 10,
+    backgroundColor:"blue"
+  },
+  subtext:{
+    fontSize: 12,
+  }
 });
